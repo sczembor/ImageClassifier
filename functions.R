5a97320fdd8f7aa2d86a4b0c8da82b4f569c37e3
Stanislaw Czembor <stanislawczembor@Air-Stanislaw.home>
2020-12-04 17:10:05 +0100

    implemenation of pca, fda, fixed knn, validation and parameter tuning

--- functions.R
+++ functions.R
@@ -27,6 +27,23 @@ loading_img <- function(directory)
   setwd(original_directory)
   return(results)
 }
+pca <- function(data)
+{
+  #step 1. calculationg the mean of each column
+  vector_ones <- matrix(1.0, nrow = nrow(data), ncol = 1)
+  col_means <- matrix(colMeans(data),nrow = 1)
+  data.mean <- vector_ones %*% col_means
+  #substracting the mean
+  data.scaled = data - data.mean
+  #computing the cov matrix
+  Sigma = data.scaled%*%t(data.scaled)/(ncol(data.scaled)-1)
+  Eigen = eigen(Sigma)
+  D = Eigen$values
+  P = Eigen$vectors
+  pca_results <- list('data.mean' = data.mean, 'P' = P, 'D' = D)
+  return(pca_results)
+}
+
 
 FDA <- function(data)
 {
@@ -50,28 +67,138 @@ FDA <- function(data)
     data[i,1:ncol(data)-1] = data[i,1:ncol(data)-1] - class_mean[data[i,ncol(data)],]
   }
   #calculating within-class variance Sw
-  Sw = 0
+  Sw = matrix(c(0),nrow= (ncol(data)-1),ncol = (ncol(data)-1))
   index = 1
   for(i in 1:dim(a))
   {
     for(j in index:(index+as.vector(a)[i]-1))
     {
-      print(paste("i =",i,", j=",j,"index=",index, sep=" "))
-      Sw = Sw + matrix(data = data[index,], nrow = 1) %*% t(matrix(data = data[index,], nrow = 1))
+      Sw = Sw + (t(matrix(data = data[index,-dim(data)[2]], nrow = 1)) %*% matrix(data = data[index,-dim(data)[2]], nrow = 1))
     }
     index = index + as.vector(a)[i]
     
   }
-  #TODO calculatin between-class variance Sb
-  # Sw = 0
-  # index = 1
-  # for(i in 1:dim(a))
-  # {
-  #   Sw = Sw + data[index:index+as.vector(a)[i],] %*% t(data[index:index+as.vector(a)[i],])
-  #   index = index + as.vector(a)[i]
-  # }
-  
-  
-  results <- list('mean' = mean)
+  Sb = matrix(c(0),nrow=24,ncol = 24)
+  for(i in 1:dim(a))
+  {
+    Sb = Sb + as.vector(a)[i] *( t((class_mean[i,] - global_mean)) %*% ((class_mean[i,] - global_mean)) )
+  }
+  eigen = eigen(solve(Sw)%*%Sb)
+  P = eigen$vectors
+  D = eigen$values
+
+  fda_results <- list('data.mean' = global_mean, 'P' = P, 'D' = D)
+  return(fda_results)
+}
+threshold <- function(data, metric)
+{
+  data = data[,-ncol(data)]
+  distances <- matrix(0,nrow(data),nrow(data))
+  if(metric == "euclidan")
+  {
+    for(i in 1:nrow(data))
+    {
+      for(j in 1:nrow(data))
+      {
+        distances[i,j] = euclidan(data[i,],data[j,])
+      }
+    }
+  }
+  else if(metric == "manhattan")
+  {
+    for(i in 1:nrow(data))
+    {
+      for(j in 1:nrow(data))
+      {
+        distances[i,j] = manhattan(data[i,],data[j,])
+      }
+    }
+  }
+  else if(metric == "cosine")
+  {
+    for(i in 1:nrow(data))
+    {
+      for(j in 1:nrow(data))
+      {
+        distances[i,j] = cosine(data[i,],data[j,])
+      }
+    }
+  }
+  else
+  {
+    print('Wrong metric argument! Choose between: "euclidan", "manhattan", "cosine"')
+  }
+  return(distances)
+}
+euclidan <- function(x1,x2)
+{
+  return(sqrt(sum((x1 - x2) ^ 2)))
+}
+manhattan <- function(x1,x2)
+{
+  return(sum(abs(x1-x2)))
+}
+cosine <- function(x1,x2)
+{
+  return(sum(x1*x2)/(sqrt(sum(x1^2))*sqrt(sum(x2^2))))
+}
+my_knn <- function(img, training_data, k, threshold)
+{
+  training_data = matrix(unlist(training_data), nrow = nrow(training_data))
+  #step1. transforming an img to a 1 row matrix
+  vector_ones <- matrix(c(1), nrow = nrow(training_data), ncol = 1)
+  img_matrix <- vector_ones%*%matrix(unlist(img), nrow = 1)
+  result1 <- img_matrix-training_data[,1:ncol(training_data)-1]
+  result2 <- result1 %*% t(result1)
+  distances <- matrix(nrow = nrow(training_data), ncol = 1)
+  for(i in 1:nrow(training_data))
+  {
+    distances[i,] = sqrt(result2[i,i])
+  }
+  distances = cbind(distances, training_data[,ncol(training_data)])
+  distances.sorted = distances[order(distances[,1],decreasing = F),]
+  k_nn = distances.sorted[1:k, 2]
+  img.label = names(which.max(table(k_nn)))
+  if(distances[1] > threshold) img.label = 0#threshold
+  results <- list('img.label' = img.label)
+  return(results)
   
 }
+KNN <- function(img, training_data, k, threshold , metric)
+{
+  
+  neighbours <- matrix(Inf, nrow = nrow(training_data), ncol = 1)
+  if(metric == "euclidan")
+  {
+    for(i in 1:nrow(training_data))
+    {
+      neighbours[i,] = euclidan(img, training_data[i,-ncol(training_data)])
+    }
+  }
+  else if(metric == "manhattan")
+  {
+    for(i in 1:nrow(data))
+    {
+      neighbours[i,] = manhattan(img, training_data[i,-ncol(training_data)])
+    }
+  }
+  else if(metric == "cosine")
+  {
+    for(i in 1:nrow(data))
+    {
+      neighbours[i,] = cosine(img, training_data[i,-ncol(training_data)])
+    }
+  }
+  else
+  {
+    print('Wrong metric argument! Choose between: "euclidan", "manhattan", "cosine"')
+  }
+  neighbours = cbind(neighbours, training_data[,ncol(training_data)])
+  neighbours_sorted = neighbours[order(neighbours[,1],decreasing = F),]
+  k_nn = neighbours_sorted[1:k,2]
+  img_label = names(which.max(table(k_nn)))
+  if(neighbours_sorted[1,1] > threshold) img_label = 0#threshold
+  
+  return(img_label)
+}
+
--- main.R
+++ main.R
@@ -11,44 +11,224 @@ source('functions.r')
 data = loading_img(paste(getwd(),"Training", sep = "/"))
 A = data$data
 B = data$labels
-data.labeled = cbind(A,B)
-dim(data.labeled)
-
-#within class covariance
-data <- data.labeled
-a <- table(data[,ncol(data)])
-class_mean = matrix(data = c(0), nrow = dim(a), ncol = ncol(data) - 1)
-global_mean = matrix(data = c(0), nrow = 1, ncol = ncol(data) - 1)
-for(i in 1:nrow(data))
+data_labeled = data.frame(cbind(A,B))
+acc = matrix(0, 2, 10)
+#obtaining the optimal K and similarity metric
+for(i in 1:4)
 {
-  class_mean[data[i,ncol(data)],] = class_mean[data[i,ncol(data)],] + data[i,1:ncol(data)-1]
-}
-for(i in 1:dim(a))
-{
-  global_mean = global_mean + class_mean[i,]
-  class_mean[i,] = class_mean[i,]/as.vector(a)[i]
-}
-global_mean = global_mean / dim(data)[1]
-#normalizin the data, by substracting the mean of each class
-for(i in 1:nrow(data))
-{
-  data[i,1:ncol(data)-1] = data[i,1:ncol(data)-1] - class_mean[data[i,ncol(data)],]
-}
-#calculating within-class variance Sw
-
-Sw = 0
-index = 1
-for(i in 1:dim(a))
-{
-  for(j in index:(index+as.vector(a)[i]-1))
-  {
-    print(paste("i =",i,", j=",j,"index=",index, sep=" "))
-    Sw = Sw + matrix(data = data[index,], nrow = 1) %*% t(matrix(data = data[index,], nrow = 1))
-  }
-  index = index + as.vector(a)[i]
+  #sampling the data
+  #it is very important to run the FDA and PCA only on training data so we
+  #do not affect the results of the test
+  str = stratified(data_labeled, paste('X',108001, sep=''),4,bothSets = T)
+  train_data = str$SAMP1[,-108001]
+  train_labels = str$SAMP1[,108001]
+  test_data = str$SAMP2[,-108001]
+  test_labels = str$SAMP2[,108001]
   
+  #casting to matrix data type
+  train_data = matrix(unlist(train_data), nrow = nrow(train_data))
+  train_labels = matrix(unlist(train_labels), nrow = nrow(train_labels))
+  test_data = matrix(unlist(test_data), nrow = nrow(test_data))
+  test_labels = matrix(unlist(test_labels), nrow = nrow(test_labels))
+  
+  #running pca on only the training set
+  pca_results = pca(train_data)
+  pca_eigenvalues = pca_results$D
+  pca_eigenvectors = pca_results$P
+  pca_data_mean = pca_results$data.mean
+  
+  ######centering and projecting train data
+  train_data_scaled = train_data - pca_data_mean
+  pca_cum_var = cumsum(pca_eigenvalues)/sum(pca_eigenvalues)
+  pca_cum_var = data.frame(
+    'pca' = 1:length(pca_cum_var),
+    'cum.variance' = pca_cum_var
+  )
+  pca_cum_var
+  pca_eigenvectors = t(train_data_scaled)%*%pca_eigenvectors
+  train_data_projected = train_data_scaled %*% pca_eigenvectors[,1:24]#nubmer of calss - 1
+  
+  #plotting the variance
+  ggplot(data=pca_cum_var, aes(x=pca,y=cum.variance))+geom_line()+geom_point()+scale_x_continuous(breaks = seq(0, 150, by = 4)) +
+    scale_y_continuous(breaks = seq(0, 1, by = 0.1))
+  
+  #fda
+  data <- cbind(train_data_projected,train_labels)
+  fda_results <- FDA(data)
+  fda_eigenvalues = fda_results$D
+  fda_eigenvectors = fda_results$P
+  fda_data_mean = fda_results$data.mean
+  
+  ######centering and projecting the data
+  #fda_data_mean_broadcasted = matrix(1, nrow = dim(data)[1], ncol = 1) %*% fda_data_mean
+  #data_scaled = data[,-ncol(data)] - fda_data_mean_broadcasted
+  fda_cum_var  = cumsum(fda_eigenvalues)/sum(fda_eigenvalues)
+  train_data_projected = data[,-ncol(data)] %*% fda_eigenvectors[,1:8]
+  train_data_projected = cbind(train_data_projected, data[,ncol(data)])
+  
+  fda_cum_var  = cumsum(fda_eigenvalues)/sum(fda_eigenvalues)
+  fda_cum_var = data.frame(
+    'fda' = 1:length(fda_cum_var),
+    'cum.variance' = fda_cum_var
+  )
+  fda_cum_var
+  #plotting the variance
+  ggplot(data=fda_cum_var, aes(x=fda,y=cum.variance))+geom_line()+geom_point()+scale_x_continuous(breaks = seq(0, 150, by = 4)) +
+    scale_y_continuous()
+  plot(fda_cum_var)
+  
+  #transforming the test data with the same parameters as train data
+  test_data_scaled = test_data - pca_data_mean[1:nrow(test_data),]
+  test_data_projected = test_data_scaled %*% pca_eigenvectors[,1:24]
+  test_data_projected = test_data_projected %*% fda_eigenvectors[,1:8]
+  
+  #threshold
+  metrics <- c('euclidan', 'manhattan', 'cosine')
+  for(p in 1:2)
+  {
+    distance_matrix = threshold(train_data_projected,metrics[p])
+    threshold1 = mean(distance_matrix)/3
+    print(threshold1)
+    
+    hits = matrix(0, nrow = 1, ncol = 10)
+    for(j in 1:nrow(test_data))
+    {
+      for(k in 1:10)
+      {
+        result = KNN(test_data_projected[j,], train_data_projected, k, threshold1, metrics[p])
+        if(strtoi(result) == test_labels[j,])
+        {
+          hits[,k] = hits[,k] + 1
+        }
+      }
+    }
+    acc[p,] = acc[p,] + hits
+  }
 }
+acc
+acc = acc / 200
 
+#plotting with ggplot
+eucildan_acc = data.frame(
+  'K' = 1:dim(acc)[2],
+  'acc' = acc[1,]
+)
+
+p <- ggplot(data=eucildan_acc , aes(x=K,y=acc))+geom_line()+geom_point()+scale_x_continuous(breaks = seq(0, 10, by = 1))
+p <- p + labs(title = "Accuracy for given K nearest neighbours",
+              subtitle = "Similarity metric: Euclidan Distance")
+p
+manhattan_acc = data.frame(
+  'K' = 1:dim(acc)[2],
+  'acc' = acc[2,]
+)
+
+b <- ggplot(data=manhattan_acc , aes(x=K,y=acc))+geom_line()+geom_point()+scale_x_continuous(breaks = seq(0, 10, by = 1))
+b <- b + labs(title = "Accuracy for given K nearest neighbours",
+              subtitle = "Similarity metric: Manhattan Distance")
+b
+
+#ploting with standard function
+par(mfrow=c(2,1))
+plot(
+  pch = 19,         # PEŁNE KÓŁKO
+  cex = 1.0,        # SKALOWANIE
+  col = "#cc0000",  # CZEROWNY
+  acc[1,], 
+  type = 'b',
+  xlim = c(0, 10), 
+  ylim = c(0, 1),
+  xlab = 'K',
+  ylab = 'accuracy',
+  main = "euclidan distance"
+)
+plot(
+  pch = 19,         # PEŁNE KÓŁKO
+  cex = 1.0,        # SKALOWANIE
+  col = "#0000cc",  # NIEBIESKI
+  acc[2,], 
+  type = 'b',
+  xlim = c(0, 10), 
+  ylim = c(0, 1),
+  xlab = 'K',
+  ylab = 'accuracy',
+  main = "manhattan distance"
+)
+par(mfrow=c(1,1))
+
+#testing the threshold
+  #sampling the data
+  
+  threshold.data = cbind(A,B)
+  train_data = threshold.train.data = threshold.data[7:150,-108001]
+  train_labels = threshold.test.label = threshold.data[1:6, 108001]
+  test_data = threshold.data[1:6, -108001]
+  test_labels = threshold.data[1:6, 108001]
+  
+  #running pca on only the training set
+  pca_results = pca(train_data)
+  pca_eigenvalues = pca_results$D
+  pca_eigenvectors = pca_results$P
+  pca_data_mean = pca_results$data.mean
+  
+  ######centering and projecting train data
+  train_data_scaled = train_data - pca_data_mean
+  pca_eigenvectors = t(train_data_scaled)%*%pca_eigenvectors
+  train_data_projected = train_data_scaled %*% pca_eigenvectors[,1:24]#nubmer of calss - 1
+  
+  
+  #fda
+  data <- cbind(train_data_projected,train_labels)
+  fda_results <- FDA(data)
+  fda_eigenvalues = fda_results$D
+  fda_eigenvectors = fda_results$P
+  fda_data_mean = fda_results$data.mean
+  
+  ######centering and projecting the data
+  #fda_data_mean_broadcasted = matrix(1, nrow = dim(data)[1], ncol = 1) %*% fda_data_mean
+  #data_scaled = data[,-ncol(data)] - fda_data_mean_broadcasted
+  fda_cum_var  = cumsum(fda_eigenvalues)/sum(fda_eigenvalues)
+  train_data_projected = data[,-ncol(data)] %*% fda_eigenvectors[,1:8]
+  train_data_projected = cbind(train_data_projected, data[,ncol(data)])
+  
+  #transforming the test data with the same parameters as train data
+  test_data_scaled = test_data - pca_data_mean[1:nrow(test_data),]
+  test_data_projected = test_data_scaled %*% pca_eigenvectors[,1:24]
+  test_data_projected = test_data_projected %*% fda_eigenvectors[,1:8]
+  
+  #threshold
+  denominator <- seq(2,3,0.025)
+  acc = c(rep(0,length(denominator)))
+  thresshold_value = c(rep(0,length(denominator)))
+  for(d in 1:length(denominator))#different denominators
+  {
+    distance_matrix = threshold(train_data_projected,"euclidan")
+    threshold1 = mean(distance_matrix)/denominator[d]
+    thresshold_value[d] = threshold1
+    
+    hits = 0;
+    for(j in 1:nrow(test_data))
+    {
+        result = KNN(test_data_projected[j,], train_data_projected, 4, threshold1, "euclidan")
+        if(strtoi(result) == 0)
+        {
+          hits = hits + 1
+        }
+    }
+    acc[d] = acc[d] + hits
+  }
+acc = acc / 6
+acc
+thresshold_acc = data.frame(
+  'thresshold' =  1:length(denominator),
+  'acc' = acc
+)
+
+c <- ggplot(data=thresshold_acc , aes(x=thresshold,y=acc))+geom_line()+geom_point()+scale_x_continuous(breaks = seq(0, length(denominator), by = 4))
+c <- c + labs(title = "Accuracy for different thresshold values",
+              subtitle = "Similarity metric: Euclidan Distance; K = 4")
+c
+thresshold_value[32]
 
 
 
